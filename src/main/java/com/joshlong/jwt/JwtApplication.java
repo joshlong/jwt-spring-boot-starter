8f3361955c6f56b7490c26570b137269d1ed04c0
Josh Long <josh@joshlong.com>
2020-05-12 01:23:25 -0700

    save Tue May 12 01:23:25 PDT 2020

--- src/main/java/com/joshlong/jwt/JwtApplication.java
+++ src/main/java/com/joshlong/jwt/JwtApplication.java
@@ -43,20 +43,25 @@ class JwtTokenAutoConfiguration {
 
 	@Bean
 	RouterFunction<ServerResponse> jwtTokenEndpoint(JWSSigner signer) {
-		return route().POST("/token", serverRequest -> {
-			Optional<Principal> pp = serverRequest.principal();
-			Assert.isTrue(pp.isPresent(), "the principal must be non-null!");
-			var principal = pp.get();
-			var now = Instant.now();
-			var claims = new JWTClaimsSet.Builder().issuer("http://localhost:8080").audience("http://localhost:8080")
-					.expirationTime(Date.from(now.plus(Duration.ofDays(2)))).issueTime(Date.from(now))
-					.subject(principal.getName()).build();
-			var jwsObject = new JWSObject(new JWSHeader.Builder(JWSAlgorithm.RS256).build(),
-					new Payload(claims.toJSONObject()));
-			jwsObject.sign(signer);
-			var token = jwsObject.serialize();
-			return ServerResponse.ok().body(token);
-		}).build();
+		return route()//
+				.POST("/token", serverRequest -> {
+					Optional<Principal> pp = serverRequest.principal();
+					Assert.isTrue(pp.isPresent(), "the principal must be non-null!");
+					var principal = pp.get();
+					var now = Instant.now();
+					var claims = new JWTClaimsSet.Builder()//
+							.issuer("http://localhost:8080")//
+							.audience("http://localhost:8080")//
+							.expirationTime(Date.from(now.plus(Duration.ofDays(2))))//
+							.issueTime(Date.from(now))//
+							.subject(principal.getName())//
+							.build();
+					var jwsObject = new JWSObject(new JWSHeader.Builder(JWSAlgorithm.RS256).build(),
+							new Payload(claims.toJSONObject()));
+					jwsObject.sign(signer);
+					var token = jwsObject.serialize();
+					return ServerResponse.ok().body(token);
+				}).build();
 	}
 
 }
