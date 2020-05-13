903a65426b3c165e349d73bc1cc934ede396e894
Josh Long <josh@joshlong.com>
2020-05-13 14:03:29 -0700

    save Wed May 13 14:03:29 PDT 2020

--- jwt-spring-boot-starter/src/main/java/com/joshlong/jwt/Jwt.java
+++ jwt-spring-boot-starter/src/main/java/com/joshlong/jwt/Jwt.java
@@ -3,19 +3,22 @@ package com.joshlong.jwt;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 /**
- *
- *
- *
  * <p>
  * Supports a custom DSL for plugging <a href=
  * "https://docs.spring.io/spring-security/site/docs/5.4.0-M1/reference/html5/#jc-custom-dsls">
  * in JWT configuration for Servlet-based </a> or Spring Webflux-based applications.
  * </P>
+ *
+ * Most of this code works because I stole code, time and ideas from Spring Security team
+ * members Rob Winch and Josh Cummings. <EM>Thank you!</EM>
+ *
+ * @author Rob Winch
+ * @author Josh Long
+ * @author Josh Cummings
  */
 public class Jwt {
 
