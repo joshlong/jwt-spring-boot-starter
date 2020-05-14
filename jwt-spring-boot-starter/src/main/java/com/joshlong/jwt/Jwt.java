14fd5b3a0f98b6d307f75baf4a3b91f345be025c
Josh Long <josh@joshlong.com>
2020-05-13 21:22:21 -0700

    save Wed May 13 21:22:21 PDT 2020

--- jwt-spring-boot-starter/src/main/java/com/joshlong/jwt/Jwt.java
+++ jwt-spring-boot-starter/src/main/java/com/joshlong/jwt/Jwt.java
@@ -1,6 +1,5 @@
 package com.joshlong.jwt;
 
-import lombok.Data;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
